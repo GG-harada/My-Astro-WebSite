f69ad974ee01d235f9168e2048342e06854ac9b9
yasunori harada <y.harada@grow-group.jp>
2024-01-15 11:46:55 +0900

    [update]finished_tutorial

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+// import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+// export async function GET() {
+//     return rss({
+//         title: 'Astro-test | AstroAstroAstro',
+//         description: 'Astroを学ぶ旅',
+//         site: 'https://my-astro-training.netlify.app/',
+//         items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+//         customData: `<language>ja-jp</language>`,
+//     });
+// }
